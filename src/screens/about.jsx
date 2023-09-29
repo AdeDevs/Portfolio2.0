c581825bc143e541428998185c16a9545df874cd
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-09-29 13:38:35 +0100

    Updated

--- src/screens/about.jsx
+++ src/screens/about.jsx
@@ -1,24 +1,80 @@
+import { Fade } from "react-reveal";
 import "../styles/about.css";
-import '../styles/home.css';
+import "../styles/home.css";
 
 function AboutScreen() {
   return (
     <div className="about-container">
+      <div className="about">
+        <Fade top>
+          <h1>i am a frontend developer</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore at
+            quidem doloremque consequuntur recusandae. Sint incidunt quidem.
+          </p>
+          </Fade>
+              <div className="testing-box">
+              <div className="skill-box first">
+                <span className="name">HTML</span>
 
-    <div className="about">
-        <h1>i am a frontend developer</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore at quidem doloremque consequuntur recusandae. Sint incidunt quidem.</p>
+                <div className="skill-bar">
+                  <span className="skill-per html">
+                    <span className="tooltip">99%</span>
+                  </span>
+                </div>
+              </div>
 
-        <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JAVASCRIPT</li>
-            <li>REACTJS</li>
-            <li>VUEJS</li>
-            <li>DART</li>
-        </ul>
-    </div>
+              <div className="skill-box">
+                <span className="name">CSS</span>
 
+                <div className="skill-bar">
+                  <span className="skill-per css">
+                    <span className="tooltip">80%</span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <span className="name">JAVASCRIPT</span>
+
+                <div className="skill-bar">
+                  <span className="skill-per javascript">
+                    <span className="tooltip">79%</span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <span className="name">REACT</span>
+
+                <div className="skill-bar">
+                  <span className="skill-per react">
+                    <span className="tooltip">69%</span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <span className="name">VUE</span>
+
+                <div className="skill-bar">
+                  <span className="skill-per vue">
+                    <span className="tooltip">50%</span>
+                  </span>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <span className="name">TYPESCRIPT</span>
+
+                <div className="skill-bar">
+                  <span className="skill-per typescript">
+                    <span className="tooltip">20%</span>
+                  </span>
+                </div>
+              </div>
+              </div>
+      </div>
 
       <footer>
         <p>02</p>
