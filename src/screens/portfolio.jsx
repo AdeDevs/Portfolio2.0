828118ef50a6ccc623699acb7347685246267a68
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-09-29 22:06:25 +0100

    updated

--- src/screens/portfolio.jsx
+++ src/screens/portfolio.jsx
@@ -4,7 +4,17 @@ import '../styles/portfolio.css'
 function Portfolio() {
   return (
     <div className="portfolio-container">
-      <h1>Portfolio</h1>
+     <div className="portfolio">
+      <h1>Projects</h1>
+      <section className="projects">
+        <div><a href="https://kryptyktodo.netlify.app/" target="_parent"> Task Tracker</a></div>
+        <div><a href="https://kryptykshop.web.app/" target="_parent"> E-Commerce</a></div>
+        <div><a href="https://dial247security.com/" target="_parent"> Dial247</a></div>
+        <div><a href="https://kryptykscissors.web.app/" target="_parent"> Scissors</a></div>
+        <div><a href="https://kryptykreducer.web.app/" target="_parent"> Qoutes</a></div>
+        <div><a href="https://kryptykcalculator.web.app/" target="_parent"> Calculator</a></div>
+      </section>
+     </div>
 
       <footer>
         <Fade bottom>
